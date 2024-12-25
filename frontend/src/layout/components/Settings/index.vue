38934465d3b2e295eaf0f89ecd443ca87a8cfec5
MouseJ <82129973+MouseJ@users.noreply.github.com>
2024-12-25 20:57:43 +0500

    Update index.vue

--- frontend/src/layout/components/Settings/index.vue
+++ frontend/src/layout/components/Settings/index.vue
@@ -1,29 +1,106 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "@/router";
-import { setupStore } from "@/store";
-import { setupDirective } from "@/directive";
+<script setup lang="ts">
+import { useSettingsStore } from "@/store/modules/settings";
+import { useI18n } from "vue-i18n";
 
-import "@/permission"; // Файл управления доступом и разрешениями
+import IconEpSunny from "~icons/ep/sunny";
+import IconEpMoon from "~icons/ep/moon";
 
-// Локальные SVG-иконки
-import "virtual:svg-icons-register";
+const { t } = useI18n();
 
-// Интернационализация (i18n)
-import i18n from "@/lang/index";
+/**
+ * Темная тема
+ */
+const settingsStore = useSettingsStore();
+const isDark = useDark();
+const toggleDark = () => useToggle(isDark);
 
-// Стили
-import "element-plus/theme-chalk/dark/css-vars.css"; // Тёмная тема для Element Plus
-import "@/styles/index.scss"; // Пользовательские стили
-import "uno.css"; // Стили на основе UnoCSS
+/**
+ * Смена макета
+ */
+function changeLayout(layout: string) {
+  settingsStore.changeSetting({ key: "layout", value: layout });
+  window.document.body.setAttribute("layout", settingsStore.layout);
+}
 
-const app = createApp(App);
+// Цвета темы
+const themeColors = ref<string[]>([
+  "#409EFF",
+  "#304156",
+  "#11a983",
+  "#13c2c2",
+  "#6959CD",
+  "#f5222d",
+]);
 
-// Регистрация пользовательских директив
-setupDirective(app);
+/**
+ * Смена цвета темы
+ */
+function changeThemeColor(color: string) {
+  settingsStore.changeSetting({ key: "themeColor", value: color });
+  document.documentElement.style.setProperty(
+    "--el-color-primary",
+    settingsStore.themeColor
+  );
+}
 
-// Регистрация управления состоянием (Vuex/Pinia)
-setupStore(app);
+onMounted(() => {
+  window.document.body.setAttribute("layout", settingsStore.layout);
+  document.documentElement.style.setProperty(
+    "--el-color-primary",
+    settingsStore.themeColor
+  );
+});
+</script>
 
-// Подключение маршрутов и локализации, затем монтирование приложения
-app.use(router).use(i18n).mount("#app");
+<template>
+  <div class="settings-container">
+    <h3 class="text-base font-bold">{{ t("settings.projectConfig") }}</h3>
+    <el-divider>{{ t("settings.theme") }}</el-divider>
+
+    <div class="flex justify-center" @click.stop>
+      <el-switch
+        v-model="isDark"
+        @change="toggleDark"
+        inline-prompt
+        :active-icon="IconEpMoon"
+        :inactive-icon="IconEpSunny"
+        active-color="var(--el-fill-color-dark)"
+        inactive-color="var(--el-color-primary)"
+      />
+    </div>
+
+    <el-divider>{{ t("settings.interfaceSettings") }}</el-divider>
+    <div class="py-[8px] flex justify-between">
+      <span class="text-xs">{{ t("settings.enableTagsView") }}</span>
+      <el-switch v-model="settingsStore.tagsView" />
+    </div>
+
+    <div class="py-[8px] flex justify-between">
+      <span class="text-xs">{{ t("settings.fixedHeader") }}</span>
+      <el-switch v-model="settingsStore.fixedHeader" />
+    </div>
+
+    <div class="py-[8px] flex justify-between">
+      <span class="text-xs">{{ t("settings.sidebarLogo") }}</span>
+      <el-switch v-model="settingsStore.sidebarLogo" />
+    </div>
+
+    <el-divider>{{ t("settings.themeColors") }}</el-divider>
+
+    <ul class="w-full space-x-2 flex justify-center py-2">
+      <li
+        class="inline-block w-[30px] h-[30px] cursor-pointer"
+        v-for="(color, index) in themeColors"
+        :key="index"
+        :style="{ background: color }"
+        @click="changeThemeColor(color)"
+      ></li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.settings-container {
+  padding: 16px;
+}
+</style>
