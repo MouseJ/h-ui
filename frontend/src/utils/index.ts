4103e6bbce76da0d817f6aa043ffe6cd13df90ff
MouseJ <82129973+MouseJ@users.noreply.github.com>
2024-12-25 19:18:17 +0500

    Update index.ts

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,28 +1,28 @@
 /**
- * Check if an element has a class
- * @param {HTMLElement} ele
- * @param {string} cls
- * @returns {boolean}
+ * Проверяет, содержит ли элемент указанный класс
+ * @param {HTMLElement} ele Элемент DOM
+ * @param {string} cls Имя класса
+ * @returns {boolean} Возвращает true, если класс найден, иначе false
  */
-export function hasClass(ele: HTMLElement, cls: string) {
+export function hasClass(ele: HTMLElement, cls: string): boolean {
   return !!ele.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
 }
 
 /**
- * Add class to element
- * @param {HTMLElement} ele
- * @param {string} cls
+ * Добавляет класс к элементу
+ * @param {HTMLElement} ele Элемент DOM
+ * @param {string} cls Имя класса
  */
-export function addClass(ele: HTMLElement, cls: string) {
+export function addClass(ele: HTMLElement, cls: string): void {
   if (!hasClass(ele, cls)) ele.className += " " + cls;
 }
 
 /**
- * Remove class from element
- * @param {HTMLElement} ele
- * @param {string} cls
+ * Удаляет класс из элемента
+ * @param {HTMLElement} ele Элемент DOM
+ * @param {string} cls Имя класса
  */
-export function removeClass(ele: HTMLElement, cls: string) {
+export function removeClass(ele: HTMLElement, cls: string): void {
   if (hasClass(ele, cls)) {
     const reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
     ele.className = ele.className.replace(reg, " ");
@@ -30,10 +30,11 @@ export function removeClass(ele: HTMLElement, cls: string) {
 }
 
 /**
- * @param {string} path
- * @returns {Boolean}
+ * Проверяет, является ли путь внешним (ссылкой на внешний ресурс)
+ * @param {string} path Путь для проверки
+ * @returns {boolean} Возвращает true, если путь является внешним, иначе false
  */
-export function isExternal(path: string) {
+export function isExternal(path: string): boolean {
   const isExternal = /^(https?:|http?:|mailto:|tel:)/.test(path);
   return isExternal;
 }
